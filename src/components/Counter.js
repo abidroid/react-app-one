c85c4ac9c44891987a7250a622fe527346b09b0d
Muhammad Abid <abidroid1@gmail.com>
2025-04-04 09:56:01 +0500

    Counter

---
+++ src/components/Counter.js
@@ -0,0 +1,18 @@
+function Counter() {
+
+    return (
+
+        <div className="counter-container">
+
+            <h1 className="counter-heading"></h1>
+
+            <div className="button-container">
+
+                <button className="counter-button">Add 1</button>
+                <button className="counter-button">Subtract 1</button>
+            </div>
+        </div>
+    );
+}
+
+export default Counter;
