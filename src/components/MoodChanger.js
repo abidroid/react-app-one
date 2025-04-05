f4c3b4307a8a9113aeabdcc065efb2b12c8926d6
Muhammad Abid <abidroid1@gmail.com>
2025-04-05 23:49:12 +0500

    logic

--- src/components/MoodChanger.js
+++ src/components/MoodChanger.js
@@ -1,6 +1,14 @@
 
 const MoodChanger = () => {
+    const moods = ['😊', '😢', '😡', '😴', '🤪', '😍', '😎'];
 
+    const [mood, setMood] = React.useState('😊');
+
+    const changeMood = () => {
+        const randomIndex = Math.floor( Math.random() * moods.length);
+
+        setMood(moods[randomIndex]);
+    }
 }
 
 export default MoodChanger;
