c74846873b58961429099e66d75e7a5f610bd318
Muhammad Abid <abidroid1@gmail.com>
2025-04-05 23:16:05 +0500

    fixes

--- src/components/BackgroundChange.js
+++ src/components/BackgroundChange.js
@@ -1,30 +1,29 @@
+import React from "react";
 
 function BackgroundChange() {
 
-    const [bgColor, setBgColor] = React.useState('yellow')
-
-
+    const [bgColor, setBgColor] = React.useState('yellow');
     return (
 
         <div className="container"
-        
+
             style={{
-                backgroundColor: {bgColor},
+                backgroundColor: bgColor,
                 height: "100vh"
             }}
         >
-            <button 
-            onClick={()=> setBgColor("red")}
-            className="btn">Red</button>
-            <button 
-            onClick={()=> setBgColor("green")}
-            className="btn">Green</button>
-            <button 
-            onClick={()=> setBgColor("blue")}
-            className="btn">Blue</button>
+            <button
+                onClick={() => setBgColor("red")}
+                className="btn">Red</button>
+            <button
+                onClick={() => setBgColor("green")}
+                className="btn">Green</button>
+            <button
+                onClick={() => setBgColor("blue")}
+                className="btn">Blue</button>
         </div>
     );
 
 }
 
-export  default BackgroundChange;
+export default BackgroundChange;
