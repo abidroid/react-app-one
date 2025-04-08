0cbc3f4ff527efb522dd6797de0cbad4b65b4b88
Muhammad Abid <abidroid1@gmail.com>
2025-04-08 22:35:01 +0500

    fixes

--- src/components/ImageDisplay.js
+++ src/components/ImageDisplay.js
@@ -1,6 +1,8 @@
 import abidse from '../assets/images/abidse.png'
 function ImageDisplay() {
 
+
+    const abidImageUrl = 'https://avatars.githubusercontent.com/u/17814795?v=4';
     return (
         <div>
             <h1>Image Display in 3 ways</h1>
@@ -10,7 +12,19 @@ function ImageDisplay() {
                 height={200}
                 alt='Abid Image'
             />
+            <h2>Using require</h2>
+            <img
+                src={require('../assets/images/abidse.png')}
+                height={200}
+                alt='Abid image require'
+            />
 
+            <h2>Image through url</h2>
+            <img
+                src='https://avatars.githubusercontent.com/u/17814795?v=4'
+                height={200}
+                alt='abid github profile'
+            />
         </div>
     );
 }
