b4b2bbaad04ec0a651ee1155d14d6a7a059fd3c6
Muhammad Abid <abidroid1@gmail.com>
2025-04-09 14:24:08 +0500

    fixes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MoodChanger from './components/MoodChanger.js';
 import SingleViewConditionalUpdate from './components/SingleViewConditionalUpdate.js';
 import ImageDisplay from './components/ImageDisplay.js';
 import VideoPlayerComponent from './components/VideoPlayerComponent.js';
+import AudioPlayComponent from './components/AudioPlayComponent.js';
 
 function Header() {
   return <h1>Welcome to React</h1>
@@ -13,7 +14,8 @@ function Header() {
 function App() {
   return (
     <>
-    <VideoPlayerComponent videoUrl='https://www.youtube.com/watch?v=5j50FNMxMR00s' />
+    <AudioPlayComponent />
+    {/* <VideoPlayerComponent videoUrl='https://www.youtube.com/watch?v=5j50FNMxMR00s' /> */}
     {/* <ImageDisplay /> */}
     {/* <SingleViewConditionalUpdate /> */}
     {/* <MoodChanger /> */}
