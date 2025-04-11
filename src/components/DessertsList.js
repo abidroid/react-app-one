379dad14d8446b8f099a5f227e90be22cf6227fa
Muhammad Abid <abidroid1@gmail.com>
2025-04-11 17:34:49 +0500

    fixes

--- src/components/DessertsList.js
+++ src/components/DessertsList.js
@@ -1,6 +1,20 @@
 
 function DessertsList(props) {
 
+    const listItems = props.data.map ( dessert => {
+
+        const txt = `${dessert.name} - ${dessert.calories}`;
+
+        return <li>{txt}</li>
+    });
+
+
+    return (
+
+        <ul>
+            <li>{listItems}</li>
+        </ul>
+    );
 
 }
 
